9e24229da940598560ab8f9820bc3763c24d45ae
ayerest <irisiseve@gmail.com>
2019-09-18 14:33:27 -0700

    finished react props and state lab

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,72 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    // console.log(`Logging something`);
+    // debugger
+    let newType = event.target.value
+    this.setState(prevState => {
+      return {
+        pets: prevState.pets,
+        filters: {
+          type: newType
+        }
+      }
+    })
+  }
+
+  // fetchPets = async () => {
+  //   let url = '/api/pets'
+  //   // debugger
+  //   //fyi could just use a ternary for this (set last word equal to the filter type)
+  //   switch(this.state.filters.type) {
+  //     case 'cat':
+  //       url = '/api/pets?type=cat';
+  //       break;
+  //     case 'dog':
+  //       url = '/api/pets?type=dog';
+  //       break;
+  //     case 'micropig':
+  //       url = '/api/pets?type=micropig'
+  //       break;
+  //     default:
+  //       url = '/api/pets'
+  //       break;
+  //   }
+  //   let response = await fetch(url)
+  //   let pets = await response.json()
+  //   console.log(pets)
+  //   this.setState({ pets })
+  //   // debugger
+  // }
+
+
+  fetchPets = () => {
+    let url = '/api/pets'
+    let type = this.state.filters.type
+    // debugger
+    //fyi could just use a ternary for this (set last word equal to the filter type)
+    url = (type !== 'all' ? (url + `?type=${type}`) : url)
+    fetch(url)
+    .then(response => response.json())
+    .then(pets =>
+    this.setState({ pets }))
+    // debugger
+  }
+
+  onAdoptPet = (petId) => {
+    debugger
+    this.setState(prevState => {
+      let updatedPets = [...prevState.pets]
+      updatedPets.forEach(pet => {
+        if (pet.id === petId) {
+          pet.isAdopted = true
+        }
+      })
+      return updatedPets
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +90,12 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType}
+                       onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets}
+                          onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,19 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
+  
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    // debugger
+    const generatePetCards =
+      
+      this.props.pets.map((pet,index) => {
+        // debugger
+         return <Pet isAdopted={pet.isAdopted} key={index} pet={pet} onAdoptPet={this.props.onAdoptPet} />
+      })
+    return <div className="ui cards">
+      {generatePetCards}
+    </div>
   }
 }
 
